e43762fe298e4b6479c2d38020ef16d530b562c1
Daniel Wall <dwall@dhcp-128-252.cu-wireless-students.carleton.ca>
2019-01-24 12:56:27 -0500

    first lab done

---
+++ labs/Lab_1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* main: generate some simple output. */
+
+int main(void)
+{
+	printf("Hello world!\n");
+	printf("C Programming is sooo much fun\n");
+	return EXIT_SUCCESS;
+}
