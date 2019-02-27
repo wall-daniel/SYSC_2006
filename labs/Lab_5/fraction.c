8cc01de3219c2078ba26b74f2e00ec8970329b94
Daniel Wall <dwall@dhcp-57-157.cu-wireless-faculty.carleton.ca>
2019-02-27 15:26:12 -0500

    Finish lab 6"

---
+++ labs/Lab_5/fraction.c
@@ -0,0 +1,103 @@
+/* fraction.c - SYSC 2006 Winter 2019 Lab 5 */
+
+#include <stdlib.h>  // abs(x)
+#include <stdio.h>   // printf
+
+#include "fraction.h"
+
+/* Print fraction f in the form a/b. */
+void print_fraction(fraction_t f)
+{
+	printf("%i/%i", f.num, f.den);
+}
+
+/* Return the greatest common divisor of integers a and b;
+   i.e., return the largest positive integer that evenly divides
+   both values.
+*/
+int gcd(int a, int b)
+{
+	int q = abs(a), p = abs(b);
+
+	int r = q % p;
+
+	while(r != 0)
+	{
+        q = p;
+        p = r;
+        r = q % p;
+	}
+
+	return p;
+}
+
+/* Return the reduced form of fraction f.
+
+   This means that:
+   (1) if the numerator is equal to 0 the denominator is always 1.
+   (2) if the numerator is not equal to 0 the denominator is always
+       positive, and the numerator and denominator have no common
+       divisors other than 1.
+
+   In other words,
+   (1) if the numerator is 0 the denominator is always changed to 1.
+   (2) if the numerator and denominator are both negative, both values
+       are made positive, or if the numerator is positive and the
+       denominator is negative, the numerator is made negative and the
+       denominator is made positive.
+   (3) the numerator and denominator are both divided by their greatest
+       common divisor.
+*/
+fraction_t reduce(fraction_t f)
+{
+    // If the num == 0, return 0/1
+    if(f.num == 0)
+    {
+        f.den = 1;
+        return f;
+    }
+
+    // Reduce the fraction
+    int divisor = gcd(f.num, f.den);
+    f.num /= divisor;
+    f.den /= divisor;
+
+    // Make sure either den or non are negative
+    if(f.den < 0)
+    {
+        f.num = -f.num;
+        f.den = -f.den;
+    }
+
+    return f;
+}
+
+/* Return a fraction with numerator a and denominator b.
+   Print an error message and terminate the calling program via exit()
+   if the denominator is 0.
+   The fraction returned by this function is always in reduced form
+   (see documentation for reduce).
+*/
+fraction_t make_fraction(int a, int b)
+{
+	fraction_t f = {a, b};
+    return reduce(f);
+}
+
+/* Return the sum of fractions f1 and f2.
+   The fraction returned by this function is always in reduced form
+   (see documentation for reduce).
+ */
+fraction_t add_fractions(fraction_t f1, fraction_t f2)
+{
+	return make_fraction(f1.num * f2.den + f1.den * f2.num, f1.den * f2.den);
+}
+
+/* Return the product of fractions f1 and f2.
+   The fraction returned by this function is always in reduced form
+   (see documentation for reduce).
+ */
+fraction_t multiply_fractions(fraction_t f1, fraction_t f2)
+{
+	return make_fraction(f1.num * f2.num, f1.den * f2.den);
+}
---
+++ labs/Lab_6/polynomial.c
@@ -0,0 +1,90 @@
+/* polynomial.c - SYSC 2006 Winter 2019 Lab 6 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+
+#include "polynomial.h"
+
+/* Print a polynomial term in the form ax^n; for example,
+ * a term with coefficient 4 and exponent 5 is printed as 4x^5.
+ */
+void print_term(term_t *term)
+{
+    printf("%dx^%d", term->coeff, term->exp);
+}
+
+/* Make a polynomial term with coefficient coeff and exponent exp
+ * and return the pointer to the term.
+ *
+ * Memory for the term is allocated from the heap.
+ * Terminate the program via assert if:
+ * (1) coeff is 0;
+ * (2) exp is negative;
+ * (3) malloc fails.
+ */
+term_t *make_term(int coeff, int exp)
+{
+    term_t *term;
+    term = malloc(sizeof(term_t));
+    term->coeff = coeff;
+    term->exp = exp;
+    return term;
+}
+
+/* Return the value of a polynomial term; i.e., return ax^n for a term with
+ * coefficient a and exponent n when evaluated at x.
+ * Example: if a term has coefficient 3 and exponent 2 and x is 4.0,
+ * this function returns 3(4.0^2), which is 48.0.
+ */
+double eval_term(term_t *term, double x)
+{
+    return term->coeff * pow(x, term->exp);
+}
+
+/* Make a polynomial that has 0 terms and return the pointer to the polynomial.
+ * The polynomial can store up to MAX_TERMS terms (see polynomial.h).
+ *
+ * Memory for the polynomial is allocated from the heap.
+ * Terminate the program via assert if malloc fails.
+ */
+polynomial_t *make_polynomial(void)
+{
+    polynomial_t *poly = malloc(sizeof(polynomial_t));
+    assert(poly != NULL);
+    return poly;
+}
+
+/* Add a term to a polynomial.
+ *
+ * This function doesn't check if the polynomial already contains a term with the
+ * same exponent as the term being added; in other words, it's the caller's
+ * reponsibility to ensure that the polynomial doesn't have terms with duplicate
+ * exponents.
+ *
+ * Terminate the program via assert if there's no room in the polynomial for
+ * additional terms.
+ */
+void add_term(polynomial_t *poly, term_t *term)
+{
+    poly->terms[poly->num_terms] = term;
+    assert(poly->terms[poly->num_terms] != NULL);
+    poly->num_terms++;
+}
+
+/* Return the value of a polynomial when it is evaluated at x.
+ *
+ * Terminate the program via assert if the polynomial has 0 terms.
+ */
+double eval_polynomial(polynomial_t *poly, double x)
+{
+    // Check for 0 terms first
+    assert(poly->num_terms != 0);
+
+    double value = 0;
+    for(int i = 0; i < poly->num_terms; i++) {
+        value += poly->terms[i]->coeff * pow(x, poly->terms[i]->exp);
+    }
+    return value;
+}
